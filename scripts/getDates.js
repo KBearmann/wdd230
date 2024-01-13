a7d1cc1d30553761ff64268fd20530a16b0f164f
KBearmann <114293395+KBearmann@users.noreply.github.com>
2024-01-13 16:29:16 -0700

    Update getDates.js

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,1 +1,11 @@
+// getDates.js
 
+// Get the current year
+const currentYear = new Date().getFullYear();
+
+// Get the last modified date of the document
+const lastModifiedDate = document.lastModified;
+
+
+document.querySelector('footer #currentYear').textContent = currentYear;
+document.getElementById('lastModified').textContent = `Last Modified: ${lastModifiedDate}`;
