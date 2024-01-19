9031da16dd05cea36e2cc697c690b43fe5ac7226
KBearmann <114293395+KBearmann@users.noreply.github.com>
2024-01-19 15:05:02 -0700

    Update scripts.js

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,4 +1,10 @@
 // script.js
+function toggleNav() {
+    var navLinks = document.getElementById("nav-links");
+    navLinks.style.display = navLinks.style.display === "flex" ? "none" : "flex";
+    console.log("Nav display toggled");
+}
+
 
 document.addEventListener("DOMContentLoaded", function () {
     var lastModifiedDate = document.lastModified;
