45350914ff4bf6aa168987352e4f318a148a8c48
KBearmann <114293395+KBearmann@users.noreply.github.com>
2024-01-26 18:24:48 -0700

    Update DarkMode.js

--- scripts/DarkMode.js
+++ scripts/DarkMode.js
@@ -9,14 +9,25 @@ darkModeToggle.addEventListener('click', () => {
 });
 
 
-
-
 // Hamburger Menu Toggle
 const hamburgerButton = document.getElementById('hamburger');
+const closeMenuButton = document.getElementById('closeMenu');
 const nav = document.querySelector('nav');
 
-
 hamburgerButton.addEventListener('click', () => {
-    console.log('Dark Mode Button Clicked!');
+    console.log('Hamburger Button Clicked!');
     nav.classList.toggle('show-menu');
+    toggleButtons(); 
 });
+
+closeMenuButton.addEventListener('click', () => {
+    console.log('Close Menu Button Clicked!');
+    nav.classList.remove('show-menu');
+    toggleButtons();
+});
+
+function toggleButtons() {
+    hamburgerButton.style.display = nav.classList.contains('show-menu') ? 'none' : 'block';
+    closeMenuButton.style.display = nav.classList.contains('show-menu') ? 'block' : 'none';
+}
+
