a790776e8d7bb83356fdb10fa6241538b33d0447
KBearmann <114293395+KBearmann@users.noreply.github.com>
2024-01-26 19:04:06 -0700

    Update scripts.js

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,15 +1,49 @@
 // script.js
-function toggleNav() {
-    var navLinks = document.getElementById("nav-links");
-    navLinks.style.display = navLinks.style.display === "flex" ? "none" : "flex";
-    console.log("Nav display toggled");
-}
 
+// Dark Toggle
+const darkModeToggle = document.getElementById('darkModeToggle');
+const body = document.body;
 
-document.addEventListener("DOMContentLoaded", function () {
-    var lastModifiedDate = document.lastModified;
-
-    var footer = document.querySelector("footer");
-    footer.innerHTML += "<p>Last modified: " + lastModifiedDate + "</p>";
+darkModeToggle.addEventListener('click', () => {
+    console.log('Dark Mode Button Clicked!');
+    body.classList.toggle('dark-mode');
 });
 
+// Hamburger Toggle
+const hamburgerButton = document.getElementById('hamburger');
+const nav = document.querySelector('nav');
+
+hamburgerButton.addEventListener('click', () => {
+    console.log('Hamburger Button Clicked!');
+    nav.classList.toggle('show-menu');
+});
+
+// Sidebar
+document.addEventListener("DOMContentLoaded", function () {
+    var lastVisitDate = localStorage.getItem("lastVisitDate");
+    var currentDate = Date.now();
+
+    if (!lastVisitDate) {
+        displayMessage("Welcome! Let us know if you have any questions.");
+    } else {
+        var daysDifference = Math.floor((currentDate - lastVisitDate) / (1000 * 60 * 60 * 24));
+
+        if (daysDifference < 1) {
+            displayMessage("Back so soon! Awesome!");
+        } else {
+            var message = "You last visited " + daysDifference + (daysDifference === 1 ? " day" : " days") + " ago.";
+            displayMessage(message);
+        }
+    }
+
+    localStorage.setItem("lastVisitDate", currentDate.toString());
+});
+
+function displayMessage(message) {
+  
+    var sidebar = document.querySelector(".discover-sidebar");
+    sidebar.innerHTML += "<p>" + message + "</p>";
+    var paragraph = document.createElement("p");
+    paragraph.textContent = message;
+    sidebar.appendChild(paragraph);
+}
