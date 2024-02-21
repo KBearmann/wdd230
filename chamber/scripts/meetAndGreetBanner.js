8f9686eb8b84cc322d80b84fd1d71faae57cc6ad
KBearmann <114293395+KBearmann@users.noreply.github.com>
2024-02-20 20:15:43 -0700

    Update meetAndGreetBanner.js

--- chamber/scripts/meetAndGreetBanner.js
+++ chamber/scripts/meetAndGreetBanner.js
@@ -1,21 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-// Function to check if the current day 
-function isMeetAndGreetDay() {
-    var today = new Date();
-    var dayOfWeek = today.getDay(); 
-    return dayOfWeek >= 1 && dayOfWeek <= 3; 
-}
+    toggleMeetAndGreetBanner();
+});
 
-// Function to show or hide the meet and greet banner 
 function toggleMeetAndGreetBanner() {
     var banner = document.getElementById('meetAndGreetBanner');
     if (isMeetAndGreetDay()) {
-        banner.style.display =  'block';
+        banner.style.display = 'block';
     } else {
-        banner.style.display =  'none';
+        banner.style.display = 'none';
     }
 }
-
-// Call the function 
-window.onload = toggleMeetAndGreetBanner;
-});
